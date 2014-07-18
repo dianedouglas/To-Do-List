10b0545aeeedbf9e11bb715d7d84ae72725647f6
Diane Douglas & Amoreena Hagedorn <student@epicodus.com>
2014-07-18 12:19:23 -0700

    Added code to tie the tasks in to the current list. current list is set when a list is created or clicked on.

--- js/todolist2-script.js
+++ js/todolist2-script.js
@@ -1,29 +1,46 @@
 $(document).ready(function() {
-  // var allLists = [];
-  // var currentList = -1;
+  var currentList;
+  var allLists = [];
 
   $("form#create-list").submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var listName = $("input#list-name").val();
-  var list = { name: listName, tasks: [] };
+    var listName = $("input#list-name").val();
+    var list = { name: listName, tasks: [] };
 
-  $("#lists ul").append("<li>" + list.name + "</li>");
-  $("#lists").show();
-  // allLists.push(list);
-  // currentList = allLists.length-1;
+    $("#lists ul").append("<li><span>" + list.name + "</span></li>");
+    $("#lists").show();
+
+    currentList = list;
+    allLists.push(list);
+    //set the list display on the right here.
+
+    $("input#list-name").val("");
+
+      $("#tasks ul").empty();
+
+    $("#lists ul li").last().click(function(){
+      $("#tasks ul").empty();
+      // var currentListItem = $(this);
+      var index = $("#lists ul li").index($(this));
+      currentList = allLists[index];
+      currentList.tasks.forEach(function(task){
+        $("#tasks ul").append("<li>" + task + "</li>");
+      })
+    })
   })
 
   $("form#create-task").submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var taskName = $("input#task-name").val();
-  //var task = { name: listName, tasks: [] };
+    var taskName = $("input#task-name").val();
+    //var task = { name: listName, tasks: [] };
 
-  $("#tasks ul").append("<li>" + taskName + "</li>");
-  $("#tasks").show();
-  // allLists[currentList].tasks.push(taskName);
+    $("#tasks ul").append("<li>" + taskName + "</li>");
+    $("#tasks").show();
 
+    currentList.tasks.push(taskName);
+    $("input#task-name").val("");
   })
 
 
